bf1bad35a98b3e78544d12ac25dd490e33ca1834
yunhongze <yunhongze@qq.com>
2025-01-12 22:37:45 +0800

    add:some

---
+++ src/python-project/app/example-app/hello.py
@@ -0,0 +1,8 @@
+from example_lib import hello_lib
+def main():
+    print(hello_lib())
+    print("Hello from example-app!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/python-project/packages/example-lib/src/example_lib/__init__.py
@@ -0,0 +1,2 @@
+def hello_lib() -> str:
+    return "Hello from example-lib!, A library provides functions and objects for other projects to consume. Libraries are intended to be built and distributed, e.g., by uploading them to PyPI."
